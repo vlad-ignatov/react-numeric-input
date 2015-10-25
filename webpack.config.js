e63d7565a854f9fd480bdb662b3cdf90cd29e19f
vlad-ignatov <vlad.ignatov@gmail.com>
2015-10-25 22:14:45 +0200

    Add the dependencies

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+    entry: {
+        'examples/examples.js': './examples/examples.jsx'
+    },
+    output: {
+        path: './',
+        filename: '[name]'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loaders: [
+                    'jsx-loader?stripTypes',
+                    'babel?stage=0'
+                ],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.less$/,
+                loaders: [
+                    'style-loader',
+                    'css-loader',
+                    'less-loader'
+                ]
+            }
+        ]
+    },
+    externals : {
+        'react-dom' : 'ReactDOM'
+    },
+    resolve : {
+        extensions : [ '', '.js', '.jsx' ]
+    }
+};
