d5ec8de5c3d9c23e52d05db61b2370a8591415fa
vlad-ignatov <vlad.ignatov@gmail.com>
2016-01-23 15:10:08 +0200

    Latest examples

--- examples/examples.jsx
+++ examples/examples.jsx
@@ -1,11 +1,20 @@
-/* global ReactDOM, $, hljs */
-import NumericInput from '../src/NumericInput.jsx';
+/* global $, hljs, NumericInput */
+import React from "react"
+import ReactDOM from "react-dom"
+// import NumericInput from '../index.js';
+
 
 $(function() {
     $('script.jsx').each(function(i, s) {
-        var div = $('<div/>'), props = Function('return (' + $(s).text() + ')')();
+        var div   = $('<div/>'),
+            props = Function('return (' + $(s).text() + ')')();
         $(s).replaceWith(div);
-        ReactDOM.render(<NumericInput {...props}/>, div[0]);
+        var widget = ReactDOM.render(
+            React.createElement(NumericInput, props),
+            div[0]
+        );
+        console.log(widget)
+        div.data("widget", widget)
     });
 
     hljs.configure({ useBR : false });
