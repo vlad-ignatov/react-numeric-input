b08d04a47ab71513b78c1888d0aceb32cb0ca648
vlad-ignatov <vlad.ignatov@gmail.com>
2018-01-13 10:10:12 -0500

    Add preview for strict and noStyle props

--- examples/v2.1.0/examples.js
+++ examples/v2.1.0/examples.js
@@ -160,7 +160,9 @@ return /******/ (function(modules) { // webpackBootstrap
 	                pattern: { value: "[0-9].[0-9][0-9]", on: false },
 	                title: { value: "The title attr", on: false },
 	                snap: { value: true, on: false },
-	                inputmode: { value: "numeric", on: false }
+					inputmode: { value: "numeric", on: false },
+					strict: { value: true, on: false },
+					noStyle: { value: true, on: false }
 	                // library
 	            }
 	        };
@@ -360,7 +362,28 @@ return /******/ (function(modules) { // webpackBootstrap
 	                                React.createElement(
 	                                    "tbody",
 	                                    null,
-	                                    this.renderPropEditors([{ name: "name", type: "text" }, { name: "className", type: "text" }, { name: "value", type: "text" }, { name: "min", type: "number" }, { name: "max", type: "number" }, { name: "step", type: "number", min: 0.001, step: 0.1, precision: 3 }, { name: "precision", type: "number", min: 0, max: 20 }, { name: "size", type: "number", min: 0, max: 60 }, { name: "maxLength", type: "number", min: 0, max: 20 }, { name: "disabled", type: "bool" }, { name: "readOnly", type: "bool" }, { name: "mobile", type: "bool" }, { name: "pattern", type: "text" }, { name: "title", type: "text" }, { name: "required", type: "bool" }, { name: "noValidate", type: "bool" }, { name: "inputmode", type: "text" }, { name: "snap", type: "bool" }])
+	                                    this.renderPropEditors([
+                                            { name: "name", type: "text" },
+                                            { name: "className", type: "text" },
+                                            { name: "value", type: "text" },
+                                            { name: "min", type: "number" },
+                                            { name: "max", type: "number" },
+                                            { name: "step", type: "number", min: 0.001, step: 0.1, precision: 3 },
+                                            { name: "precision", type: "number", min: 0, max: 20 },
+                                            { name: "size", type: "number", min: 0, max: 60 },
+                                            { name: "maxLength", type: "number", min: 0, max: 20 },
+                                            { name: "disabled", type: "bool" },
+                                            { name: "readOnly", type: "bool" },
+                                            { name: "mobile", type: "bool" },
+                                            { name: "pattern", type: "text" },
+                                            { name: "title", type: "text" },
+                                            { name: "required", type: "bool" },
+                                            { name: "noValidate", type: "bool" },
+                                            { name: "inputmode", type: "text" },
+                                            { name: "snap", type: "bool" },
+                                            { name: "strict", type: "bool" },
+                                            { name: "noStyle", type: "bool" }
+                                        ])
 	                                )
 	                            )
 	                        )
